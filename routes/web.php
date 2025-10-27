ef57c1104369486fd11d130079b21485c0539d59
Skylar Franz <skylar.franz@ku.edu>
2025-10-26 21:18:46 -0500

    Change Documentation folder name + Add Requirements and Architecture Document
    + Removed pictures

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/completed', function (){
     return view('completed', ['tasks' => Task::where('complete_status', true)->get()]);
 })->name('completed.index');
 
-Route::post('/create', [TaskController::class, 'create'])->name('tasks.create');// Post request link for creating a task, which routes it to the TaskController "create" function (and specific route name "tasks.create")
+Route::post('/create', [TaskController::class, 'create'])->name('tasks.create'); // Post request link for creating a task, which routes it to the TaskController "create" function (and adds specific route name "tasks.create")
 Route::delete('/delete/{task}', [TaskController::class, 'delete'])->name('tasks.delete'); // Route to delete task
 Route::patch('/update/{task}', [TaskController::class, 'update'])->name('tasks.update'); // Route to update task
 Route::patch('/complete/{task}', [TaskController::class, 'complete'])->name('tasks.complete'); // Route to complete/undo completing a task

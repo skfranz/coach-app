ade508596e2fbe998f606b95944e32cbc57740b8
Skylar Franz <skylar.franz@ku.edu>
2025-11-03 16:35:37 -0600

    Display tags in taskpage
    + Make task description only show when set
    + Add confirmation prompt to delete
    + Change styling

--- resources/views/taskpage.blade.php
+++ resources/views/taskpage.blade.php
@@ -3,17 +3,14 @@
     <!--Displays each task in its own div/box-->
     @foreach ($tasks as $task)
         <div style="display: inline-block; border-style: solid; padding: 0px 10px 10px; margin-bottom: 20px">
-            
-            <p style="font-weight: bold;">{{ $task->name }}</p> <!--Show Task Name-->
-            <p>Description: {{ $task->description }}</p>        <!--Show Task Description-->
 
-            <div style="display:flex; align-items:center; gap:10px;">
+            <div style="display:flex; float:right; margin-top: 10px; gap:10px;">
 
                 <!--Delete Form-->
                 <form action="{{ route('tasks.delete', $task) }}" method="POST"> <!--Send delete request to delete route in web.php, which goes to delete function in TaskController-->
                     @method('DELETE')   <!--Specify request method is "DELETE"-->
                     @csrf               <!--Form Security token submission-->
-                    <button type="submit">Delete</button>
+                    <button type="submit" onclick="return confirm('Are you sure you want to delete this task?');">Delete</button>
                 </form>
 
                 <!--Complete Form-->
@@ -22,8 +19,16 @@
                     @csrf
                     <button type="submit">Complete</button>
                 </form>
+            </div>
+            
+            <p style="font-weight: bold;">{{ $task->name }}</p> <!--Show Task Name-->
+            @isset($task->description)<p>Description: {{ $task->description }}</p>@endisset <!--Show Task Description (if there is one)-->
 
-            </div><br>
+            @foreach ($task->tags as $tag)
+                <div style="display: inline-block; border-style: solid; padding: 5px 5px; margin-bottom: 20px">
+                    {{ $tag->name }}
+                </div>
+            @endforeach
 
             <!-- Update Form-->
             <form action="{{ route('tasks.update', $task) }}" method="POST">

a623e62c1bfb727cfd9daa564fd463bf7f679ec7
Skylar Franz <skylar.franz@ku.edu>
2025-11-09 21:09:46 -0600

    Add basic task repeating

---
+++ app/Console/Commands/RepeatTasks.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Task;
+
+class RepeatTasks extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:repeat-task';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Get every task that repeats and replace them with an identical task with complete_status = 0.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $repeatingTasks = Task::where('repeats', true)->get();
+        foreach ($repeatingTasks as $task) {
+            $newTask = $task->replicate();
+            $newTask->created_at = now();
+            $newTask->updated_at = now();
+            if ($task->complete_status == 0) {
+                $task->delete();
+            }
+            $newTask->complete_status = 0;
+            $newTask->save();
+        }
+        return 0;
+    }
+}

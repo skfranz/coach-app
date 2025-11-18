38fa64b427b4aad910c5f836b09f889616b702a7
d944b207 <143836903+d944b207@users.noreply.github.com>
2025-11-18 11:18:06 -0600

    updates cosmetics migration

--- database/migrations/0001_01_01_000006_create_cosmetics_table.php
+++ database/migrations/0001_01_01_000006_create_cosmetics_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use App\Models\Cosmetic;
 
 return new class extends Migration
 {
@@ -11,15 +13,73 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cosmetics', function (Blueprint $table) { // Create SQL table 'tasks' and columns
+        Schema::create('cosmetics', function (Blueprint $table) { // Create SQL table 'cosmetics' and columns
             $table->id();
             $table->string('name');
             $table->string('type');
             $table->string('description')->nullable();
-            $table->boolean('purchaseable')->default(1);
+            $table->boolean('purchased_status')->default(0);
             $table->integer('price');
             $table->timestamps();
         });
+
+        DB::table('cosmetics')->insert([       // Create a default user/gamestate
+            'id' => '1',
+            'name' => 'Text Color Shop Test',
+            'type' => 'textcolor',
+            'purchased_status' => '0',
+            'price' => '100',
+        ]);
+        DB::table('cosmetics')->insert([       // Create a default user/gamestate
+            'id' => '2',
+            'name' => 'Text Color Inventory Test',
+            'type' => 'textcolor',
+            'purchased_status' => '1',
+            'price' => '100'
+        ]);
+        DB::table('cosmetics')->insert([       // Create a default user/gamestate
+            'id' => '3',
+            'name' => 'Font Shop Test',
+            'type' => 'font',
+            'purchased_status' => '0',
+            'price' => '100'
+        ]);
+        DB::table('cosmetics')->insert([       // Create a default user/gamestate
+            'id' => '4',
+            'name' => 'Font Inventory Test',
+            'type' => 'font',
+            'purchased_status' => '1',
+            'price' => '100'
+        ]);
+        DB::table('cosmetics')->insert([       // Create a default user/gamestate
+            'id' => '5',
+            'name' => 'Background Shop Test',
+            'type' => 'background',
+            'purchased_status' => '0',
+            'price' => '100'
+        ]);
+        DB::table('cosmetics')->insert([       // Create a default user/gamestate
+            'id' => '6',
+            'name' => 'Background Inventory Test',
+            'type' => 'background',
+            'purchased_status' => '1',
+            'price' => '100'
+        ]);
+        DB::table('cosmetics')->insert([
+            'id' => '7',
+            'name' => 'Coach Shop Test',
+            'type' => 'coach',
+            'purchased_status' => '0',
+            'price' => '100'
+        ]);
+        DB::table('cosmetics')->insert([
+            'id' => '8',
+            'name' => 'Coach Inventory Test',
+            'type' => 'coach',
+            'purchased_status' => '1',
+            'price' => '100'
+        ]);
+
     }
 
     /**

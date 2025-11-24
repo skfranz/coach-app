1fde3f7357da69a5a4f66232d0abf7480d8a43f3
Skylar Franz <skylar.franz@ku.edu>
2025-11-23 21:17:23 -0600

    Move shop/options to component

---
+++ resources/views/components/cosmetics.blade.php
@@ -0,0 +1,29 @@
+@foreach($cosmetics as $cosmetic)
+    <div style="display: inline-block; border-style: solid; padding: 0px 10px 10px; margin-top: 20px">
+
+        <div style="display:flex; float:right; margin-top: 10px; gap:10px;">
+
+            @if (request()->is('shop*')) <!--If on shop page, have option to buy cosmetics-->
+            <form action="{{ route('cosmetics.buy', $cosmetic) }}" method="POST">
+                @method('PATCH')
+                @csrf
+                <button type="submit">Buy</button>
+            </form>
+            @elseif (request()->is('options')) <!--Else if on options page, send form to equip cosmetics-->
+                <form action="{{ route('cosmetics.equip', $cosmetic) }}" method="POST">
+                    @method('PATCH')
+                    @csrf
+                    <button type="submit">Equip</button>
+                </form>
+            @endif
+        </div>
+
+        <!--Show Cosmetic Values-->
+        <p>{{ $cosmetic->name }}<p>
+        <p>Type: {{ $cosmetic->type }}</p>
+        @isset($cosmetic->description)<p>Description: {{ $cosmetic->description }}</p>@endisset
+        <p>Price: {{ $cosmetic->price }} coins</p>
+
+    </div>
+    <br>
+@endforeach
--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,23 +1,3 @@
 <x-layout title="Shop Page" header="Shop:">
-
-    @foreach($cosmetics as $cosmetic)
-        <div style="display: inline-block; border-style: solid; padding: 0px 10px 10px; margin-top: 20px">
-
-            <div style="display:flex; float:right; margin-top: 10px; gap:10px;">
-                <form action="{{ route('cosmetics.buy', $cosmetic) }}" method="POST">
-                    @method('PATCH')
-                    @csrf
-                    <button type="submit">Buy</button>
-                </form>
-            </div>
-
-            <p>{{ $cosmetic->name }}<p>
-            <p>Type: {{ $cosmetic->type }}</p>
-            @isset($cosmetic->description)<p>Description: {{ $cosmetic->description }}</p>@endisset
-            <p>Price: {{ $cosmetic->price }} coins</p>
-
-        </div>
-        <br>
-    @endforeach
-
+    <x-cosmetics :cosmetics="$cosmetics"></x-cosmetics>
 </x-layout>
